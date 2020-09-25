fdc71edbc2c4d2d8221ab080e0b36d0d8c4b3d29
dayefubs <dayefubs12@gmail.com>
2020-09-25 16:43:43 -0400

    Final changes

--- projects/Lecture 03/src/main.cpp
+++ projects/Lecture 03/src/main.cpp
@@ -149,8 +149,6 @@ int main() {
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback(GlDebugMessage, nullptr);
 
-	
-
 	static const GLfloat points[] = {
 		-0.5f, -0.5f, 0.5f,
 		0.5f, -0.5f, 0.5f,
@@ -164,6 +162,7 @@ int main() {
 	};
 
 	//VBO - Vertex buffer object
+
 	/* GLuint pos_vbo = 0;
 	glGenBuffers(1, &pos_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, pos_vbo);
@@ -197,6 +196,33 @@ int main() {
 	vao->AddVertexBuffer(color_vbo, {
 	{ 1, 3, GL_FLOAT, false, 0, NULL }
 		});
+
+
+
+	static const float interleaved[] = {
+	//   X	    Y	  Z	    R	  G	    B
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.3f, 0.2f, 0.5f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+		0.5f, 1.0f, 0.5f, 1.0f, 1.0f, 1.0f
+	};
+	VertexBuffer* interleaved_vbo = new VertexBuffer();
+	interleaved_vbo->LoadData(interleaved, 6 * 4);
+
+	static const uint16_t indices[] = {
+		0, 1, 2,
+		1, 3, 2
+	};
+	IndexBuffer* interleaved_ibo = new IndexBuffer();
+	interleaved_ibo->LoadData(indices, 3 * 2);
+
+	size_t stride = sizeof(float) * 6;
+	VertexArrayObject* vao2 = new VertexArrayObject();
+	vao2->AddVertexBuffer(interleaved_vbo, {
+	BufferAttribute(0, 3, GL_FLOAT, false, stride, 0),
+	BufferAttribute(1, 3, GL_FLOAT, false, stride, sizeof(float) * 3),
+		});
+	vao2->SetIndexBuffer(interleaved_ibo);
 
 	// Load our shaders
 
@@ -233,10 +259,17 @@ int main() {
 		vao->Bind();
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		vao2->Bind();
+		glDrawElements(GL_TRIANGLES, interleaved_ibo->GetElementCount(), interleaved_ibo->GetElementType(), nullptr);
+		vao->UnBind();
+
 		glfwSwapBuffers(window);
 	}
 
 	delete shader;
+	delete vao2;
+	delete interleaved_ibo;
+	delete interleaved_vbo;
 	delete vao;
 	delete posVbo;
 	delete color_vbo;
