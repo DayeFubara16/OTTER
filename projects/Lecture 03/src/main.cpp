e090e0570a9ae9f2eb349db2bb0c7053b828e7da
dayefubs <dayefubs12@gmail.com>
2020-10-05 09:43:52 -0400

    no message

--- projects/Lecture 03/src/main.cpp
+++ projects/Lecture 03/src/main.cpp
@@ -64,7 +64,7 @@ bool initGLFW() {
 	}
 
 	//Create a new GLFW window
-	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	window = glfwCreateWindow(800, 800, "Daye Fubara - 100748688", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Set our window resized callback
